a2bbdae63b11db072d980a69de49606a416df86b
agustin-ovejero <ovejeroagustin2@gmail.com>
2025-12-01 16:06:28 -0300

    agregando comentarios

--- sql/primer_tabla.py
+++ sql/primer_tabla.py
@@ -1,13 +1,16 @@
 import sqlite3
 
 conection = sqlite3.connect("produc.db")
+#
+#execute crea nuestra tabla
 conection.execute('''
+     -- creamos las estructuras de los campos
     CREATE TABLE IF NOT EXISTS produc (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT, -- esto hace que cada vez que se ingrese un producto se "actualize" o auto incremente el id
     nombre TEXT not null,
     precio REAL not null)
 ''')
 
 
-conection.commit()
+conection.commit() # commit guarda los cambios de mi tabla
 conection.close()
