5eac4d4f80294f27d551696ab294c7e9d26d66bc
agustin-ovejero <ovejeroagustin2@gmail.com>
2025-12-02 14:20:52 -0300

    actualizandos-productos-sql

--- sql/primer_tabla.py
+++ sql/primer_tabla.py
@@ -1,7 +1,9 @@
 import sqlite3
 
 conection = sqlite3.connect("produc.db")
-#
+# conection y cursor no son lo mismo. 
+#cursor tinee pinta que solamente hace nuestras consultas SQL 
+
 #execute crea nuestra tabla
 conection.execute('''
      -- creamos las estructuras de los campos
@@ -28,6 +30,17 @@ conection.commit()
 cursor = conection.cursor() # Para poder recibir los datos de mi tabla es necesario crear un cursor de mi coneción original y esta es la forma
 cursor.execute('SELECT * FROM produc')
 productos = cursor.fetchall()# con fetchall conseguimos los datos
-print(productos)
+#print(productos)
+for producto in productos:
+    print(f"ID: {producto[0]} Nombre: {producto[1]}, Precio: {producto[2]} ")
+
+# ejemplo de actulaizacioón con UPDATE
+nuevo_precio = 250.0
+id_producto = 1
+cursor.execute('UPDATE produc SET precio = ? WHERE id = ?', (nuevo_precio, id_producto))
+conection.commit()
+
+cursor.execute('SELECT * FROM produc')
+productos = cursor.fetchall()
 for producto in productos:
     print(f"ID: {producto[0]} Nombre: {producto[1]}, Precio: {producto[2]} ")
