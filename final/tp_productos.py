d430145ecf7badf82e79394ccef6fe54d841e94d
agustin-ovejero <ovejeroagustin2@gmail.com>
2025-12-03 11:02:04 -0300

    creando-tabla-y-los-menus

--- final/tp_productos.py
+++ final/tp_productos.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 coneccion = sqlite3.connect("inventario.db")
 cursor = coneccion.cursor()
 
@@ -13,3 +14,26 @@ coneccion.execute('''
     categoria TEXT NOT NULL
     )
     ''')
+
+
+menu = """
+1. Registrar nuevos productos
+2. Ver Productos
+3. Actualizar productos
+4. Eliminar Productos
+5. Busccar producto
+"""
+sub_menu_punto_cinco = """
+1. Buscar por id
+2. Buscar por nombre
+3. Buscar por categoria
+"""
+continuo = 1
+while continuo:
+    print(menu)
+    try:
+        eligio = int(input(": "))
+    except ValueError:
+        print("dato invalido")
+    
+    
